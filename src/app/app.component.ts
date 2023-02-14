da71571ed52ab06d91192c78dfdab95905da4455
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2023-02-14 15:40:42 +0530

    Added: Adding the bridgelabz logo by property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-first-app';
 
-  Website='www.bridgelabz.com';
-  data=100;
-//The ngOnInit function is specific to the Angular framework and is called when Angular is done creating the component. 
-//It should be called with any custom finalization like loading data for your component to display.
-
+  imgUrl="../assets/image.jpg";
+  bridgelabzLogo="https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg";
+  
   ngOnInit(): void{
     this.title="Hello from bridgelabz"
 
