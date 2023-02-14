37bc8544f15d2d5fec47d9079ad5fc6ff5008f80
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2023-02-14 18:15:33 +0530

    Added: Adding two way binding between view and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
-
   imgUrl="../assets/image.jpg";
   bridgelabzLogo="https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg";
-
   url="https://www.bridgelabz.com/"
+  userName: string="";
+
   
   ngOnInit(): void{
     this.title="Hello from bridgelabz"  
-  }
+    }
+
   onClick($event:any){
     console.log("Button is clicked",$event);
     window.open(this.url,"blank");
   }
+
   showUrl($event:any){
     console.log(this.imgUrl)
+    alert("Image Is located in "+ this.imgUrl)
   }
-  
+ 
 }
