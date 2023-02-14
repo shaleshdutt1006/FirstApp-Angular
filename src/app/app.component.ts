f9088fb122727e2f6ecddf19e07a7106bf50bb8e
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2023-02-14 15:08:23 +0530

    Added: Adding the Hello message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { STRING_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  Website='www.bridgelabz.com';
+  data=100;
+//The ngOnInit function is specific to the Angular framework and is called when Angular is done creating the component. 
+//It should be called with any custom finalization like loading data for your component to display.
+
+  ngOnInit(): void{
+    this.title="Hello from bridgelabz"
+
+  
+  }
 }
